76ed5ba8d3340813eea7ab45b507a99aef9305d3
Dhanushkmr <Dhanush5247@gmail.com>
2021-11-26 09:58:59 +0800

    d

--- flask_app.py
+++ flask_app.py
@@ -149,4 +149,5 @@ app = DispatcherMiddleware(server, {
     '/dash2': dash_app2.server
 })
 
-run_simple('localhost', 5000, app, use_reloader=True, use_debugger=True)
+if __name__ == '__main__':  
+    run_simple('localhost', 5000, app, use_reloader=True, use_debugger=True)
